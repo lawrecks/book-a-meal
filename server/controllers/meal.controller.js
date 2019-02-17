482c6d6e6930b4c52828f1bce3b996794f0a2b36
lawrecks <ugobugo2011@gmail.com>
2019-02-17 11:37:46 +0100

    feat(addMeal): add a new meal
    - add a new meal
    
    [Delivers #164032871]

--- server/controllers/meal.controller.js
+++ server/controllers/meal.controller.js
@@ -10,6 +10,24 @@ class mealController {
       })
       .status(200);
   }
+
+  static async addNewMeal(req, res) {
+    /*  Expect json of format 
+        {
+            name : "some food",
+            size : "large",
+            price : "some price",          
+        }   */
+
+    const newMeal = req.body;
+    const createdMeal = await mealService.addNewMeal(newMeal);
+    return res
+      .json({
+        status: 'success',
+        data: createdMeal
+      })
+      .status(201);
+  }
 }
 
 export default mealController;
