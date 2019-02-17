d8894a63bc49901cd56143c215e5826527a7785d
lawrecks <ugobugo2011@gmail.com>
2019-02-17 10:25:47 +0100

    feat(fetchMeals) : fetch all meals
    - fetch all meals using the created api
    
    [Delivers #164029527]

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// routes
+import mealRoutes from './routes/meal.route';
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +13,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   return res.status(200).send('YAY! Congratulations! Your endpoint is working');
 });
+
+// Prefix
+app.use('/api/v1/meals', mealRoutes);
 
 app.listen(3000);
 console.log('app running on port ', 3000);
